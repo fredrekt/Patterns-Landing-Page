7f27aa7bdfd6fc96c2113243f2747d56aa47880a
Fredrick Garingo <fredrickjohng6@gmail.com>
2020-09-05 22:32:33 +0800

    added the routes

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
+import Navbar from './components/Navbar';
+import Contact from './pages/Contact';
 
 const App = () => {
   return (
     <div>
       <Router>
+        <Navbar/>
         <Switch>
           <Route path="/" exact component={Home}/>
           <Route path="/about" exact component={About}/>
+          <Route path="/contact" exact component={Contact}/>
         </Switch>
       </Router>
     </div>
