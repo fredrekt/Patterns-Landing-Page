41ff4213078f550f1b3fc3ef4a1a5c686f4f5180
Fredrick Garingo <fredrickjohng6@gmail.com>
2020-09-05 22:31:15 +0800

    done added shards react to package and hamburger menu

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,42 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Nav, NavItem, Collapse } from "shards-react";
+import HamburgerMenu from 'react-hamburger-menu';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
+    const [open, setOpen] = useState(false);
+
     return (
-        <div>
-            
-        </div>
-    )
+        <Nav className="p-5 pl-5 position-absolute d-flex flex-column">
+        <NavItem>
+            <HamburgerMenu
+                isOpen={open}
+                className="cursor-pointer"
+                menuClicked={()=>setOpen(!open)}
+                width={30}
+                height={20}
+                strokeWidth={1}
+                rotate={0}
+                color='black'
+                borderRadius={0}
+                animationDuration={0.5}
+            />
+        </NavItem>
+        <Collapse open={open}>
+          <ul className="list-unstyled py-4">
+              <li className="py-2">
+                  <Link to="/about" className="h4-responsive black-text">About</Link>
+              </li>
+              <li className="py-2">
+                  <Link to="/contact" className="h4-responsive black-text">Contact</Link>
+              </li>
+              <li className="py-2">
+                  <Link to="/features" className="h4-responsive black-text">Features</Link>
+              </li>
+          </ul>
+        </Collapse>
+        </Nav>
+    );
 }
 
 export default Navbar
